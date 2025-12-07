6fa26741fe41a14c5468f56bd3a706b83f23bdc0
NeyBg23 <mathicuenta23@gmail.com>
2025-12-07 11:42:53 -0500

    correcion en config

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,3 +1,7 @@
 // URL de tu backend en producción
 const API_URL = 'https://app-service-phi.vercel.app';
 const AUTH_URL = 'https://siempregana-auth-service.vercel.app'; // tu Auth Service
+
+// 🔴 IMPORTANTE: exponerlas al objeto global para que los <script type="module"> las vean
+window.API_URL  = API_URL;
+window.AUTH_URL = AUTH_URL;
